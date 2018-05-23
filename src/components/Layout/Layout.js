c9edace82ac40031df8c6fcfa8c5b18fa302616d
Resek <matija.resek@gmail.com>
2018-05-23 14:35:13 +0200

    toggle buttons works

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,40 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SiderDrawer/SideDrawer";
 
-const layout = (props) => { 
-    return (        
-        <Fragment>
-            <Toolbar />
-            <SideDrawer />
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Fragment>
-    )
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+    
+    render () {
+    
+        return (        
+            <Fragment>
+                <Toolbar toggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerCloseHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        )
+    } 
+    
+    
 } 
 
-export default layout;
+export default Layout;
